806886def391719ce3a8db54586970d37cf8cb52
VladimirLind <lindermon@gmail.com>
2019-09-26 15:46:35 +0300

    hw01_maven module - first commit

---
+++ HW01_maven/src/main/java/HelloOtus.java
@@ -0,0 +1,24 @@
+import com.google.common.collect.ImmutableSet;
+
+import java.util.Set;
+
+public class HelloOtus {
+
+    private static final ImmutableSet<String> COLOR_NAMES = ImmutableSet.of(
+            "red",
+            "green",
+            "blue");
+
+    public static void main(String[] args) {
+        Foo foo = new Foo(COLOR_NAMES);
+        System.out.println(foo.bars);
+    }
+}
+class Foo<Bar> {
+
+    final ImmutableSet<Bar> bars;
+
+    Foo(Set<Bar> bars) {
+        this.bars = ImmutableSet.copyOf(bars); // defensive copy!
+    }
+}
