cfada6e1db58477375d0ef522dc0b37dcf4620c2
vladimirlind <lindermon@gmail.com>
2019-11-17 11:35:35 +0300

    Helper method created

--- hw06_solid/src/main/java/ru/otus/atm/ATM.java
+++ hw06_solid/src/main/java/ru/otus/atm/ATM.java
@@ -1,53 +1,22 @@
 package ru.otus.atm;
 
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 
-@FieldDefaults(level = AccessLevel.PRIVATE)
 class ATM {
 
-    Cell cell = new Cell();
+    private Cell cell = new Cell();
 
     void insertMoney(Nominal[] banknotes) {
-        Arrays.stream(banknotes).forEach(this::storeMoney);
+        Arrays.stream(banknotes).forEach(this::moneyStoreHelper);
     }
 
     void getMoney(int sum) {
         if (sum % 10 == 0) {
-            if (sum / 1000 >= 1 && cell.cellForThousands.size() >= sum / 1000) {
-                int count = sum / 1000;
-                for (int i = 0; i < count; i++) {
-                    cell.cellForThousands.remove(cell.cellForThousands.size() - 1);
-                }
-                sum = sum - 1000 * count;
-                System.out.println("Take 1000's: "+count);
-            }
-            if (sum / 100 >= 1 && cell.cellForHundreds.size() >= sum / 100) {
-                int count = sum / 100;
-                for (int i = 0; i < count; i++) {
-                    cell.cellForHundreds.remove(cell.cellForHundreds.size() - 1);
-                }
-                sum = sum - 100 * count;
-                System.out.println("Take 100's: "+count);
-            }
-            if (sum / 50 >= 1 && cell.cellForFifties.size() >= sum / 50) {
-                int count = sum / 50;
-                for (int i = 0; i < count; i++) {
-                    cell.cellForFifties.remove(cell.cellForFifties.size() - 1);
-                }
-                sum = sum - 50 * count;
-                System.out.println("Take 50's: "+count);
-            }
-            if (sum / 10 >= 1 && cell.cellForTens.size() >= sum / 10) {
-                int count = sum / 10;
-                for (int i = 0; i < count; i++) {
-                    cell.cellForTens.remove(cell.cellForTens.size() - 1);
-                }
-                sum = sum - 10 * count;
-                System.out.println("Take 10's: "+count);
-            }
+            sum = moneyGetHelper(sum, 1000, cell.cellForThousands);
+            sum = moneyGetHelper(sum, 100, cell.cellForHundreds);
+            sum = moneyGetHelper(sum, 50, cell.cellForFifties);
+            sum = moneyGetHelper(sum, 10, cell.cellForTens);
         } else {
             System.out.println("sum must be a multiple of 10!");
         }
@@ -61,15 +30,15 @@ class ATM {
             cell.cellForHundreds.clear();
             cell.cellForThousands.clear();
         } else {
-            throw new RuntimeException("No money!");
+            System.out.println("No money!");
         }
     }
 
-    public int totalMoneyAtm() {
+    int totalMoneyAtm() {
         return (cell.cellForTens.size() * 10 + cell.cellForFifties.size() * 50 + cell.cellForHundreds.size() * 100 + cell.cellForThousands.size() * 1000);
     }
 
-    private void storeMoney(Nominal banknote) {
+    private void moneyStoreHelper(Nominal banknote) {
         if (banknote instanceof Ten) {
             cell.cellForTens.add((Ten) banknote);
         } else if (banknote instanceof Fifty) {
@@ -81,5 +50,21 @@ class ATM {
         } else {
             throw new RuntimeException("Not a valid banknote!");
         }
+    }
+
+    private int moneyGetHelper(int sum, int banknoteValue, ArrayList cellType) {
+        if (sum / banknoteValue >= 1) {
+            if (cellType.size() >= sum / banknoteValue) {
+                int count = sum / banknoteValue;
+                for (int i = 0; i < count; i++) {
+                    cellType.remove(cellType.size() - 1);
+                }
+                sum = sum - banknoteValue * count;
+                System.out.println("Take " + banknoteValue + "'s: " + count);
+            } else {
+                System.out.println("Not enough " + banknoteValue + " banknotes in ATM!");
+            }
+        }
+        return sum;
     }
 }
--- hw06_solid/src/main/java/ru/otus/atm/TestATM.java
+++ hw06_solid/src/main/java/ru/otus/atm/TestATM.java
@@ -1,14 +1,27 @@
 package ru.otus.atm;
 
+import java.util.Arrays;
+
 public class TestATM {
     public static void main(String[] args) {
-        Nominal[] banknotes = { new Hundred(), new Hundred(), new Ten(), new Ten(), new Fifty(), new Ten(), new Thousand(), new Thousand()};
-        ATM atm  = new ATM();
+        Nominal[] banknotes = {new Hundred(), new Hundred(), new Ten(), new Ten(), new Fifty(), new Ten(), new Thousand(), new Thousand()};
+        Nominal[] bunchOfTens = new Ten[100];
+        Arrays.fill(bunchOfTens, new Ten());
+        ATM atm = new ATM();
         atm.insertMoney(banknotes);
+        atm.insertMoney(bunchOfTens);
         System.out.println(atm.totalMoneyAtm());
-        atm.getMoney(2220);
-        atm.getMoney(50);
+        atm.getMoney(220);
+        System.out.println("----------------");
+        atm.getMoney(60);
+        System.out.println("----------------");
+        atm.getMoney(200);
+        System.out.println("----------------");
+        atm.getMoney(1050);
+        System.out.println("----------------");
+        atm.getMoney(111);
+        System.out.println("----------------");
         atm.getAllMoney();
-
+        atm.getAllMoney();
     }
 }
