026351b3d78f7760eaa7a7d23091aa9028f35736
vladimirlind <lindermon@gmail.com>
2019-11-24 18:01:25 +0300

    tests added, readme

--- hw07_patterns/src/main/java/ru/otus/atm/TestATM.java
+++ hw07_patterns/src/main/java/ru/otus/atm/TestATM.java
@@ -5,6 +5,7 @@ public class TestATM {
         ATMDept atmDept = new ATMDept();
         atmDept.createATM("LongLake", 100);
         atmDept.createATM("Petrogradskiy", 1000);
+        atmDept.createATM("Vasilyevskiy", 5000);
         System.out.println(atmDept.getATM(0).cells.getCellsmap());
         System.out.println(atmDept.getATM(1).cells.getCellsmap());
         atmDept.getATM(0).insertMoney(10, 30);
@@ -15,9 +16,14 @@ public class TestATM {
         atmDept.getATM(1).insertMoney(50, 40);
         atmDept.getATM(1).insertMoney(100, 20);
         atmDept.getATM(1).insertMoney(1000, 30);
+        atmDept.getATM(2).insertMoney(10, 30);
+        atmDept.getATM(2).insertMoney(50, 40);
+        atmDept.getATM(2).insertMoney(100, 20);
+        atmDept.getATM(2).insertMoney(1000, 30);
         System.out.println("-------------");
         System.out.println(atmDept.getATM(0).cells.getCellsmap());
         System.out.println(atmDept.getATM(1).cells.getCellsmap());
+        System.out.println(atmDept.getATM(2).cells.getCellsmap());
         System.out.println("-------------");
         atmDept.getATM(0).printMoney(550450);
         System.out.println("-------------");
@@ -34,11 +40,14 @@ public class TestATM {
         System.out.println("-------------");
         atmDept.resetATM(0);
         atmDept.resetATM(1);
+        atmDept.resetATM(2);
         System.out.println("-------------");
         System.out.println(atmDept.getATM(0).cells.getCellsmap());
         System.out.println(atmDept.getATM(1).cells.getCellsmap());
+        System.out.println(atmDept.getATM(2).cells.getCellsmap());
         System.out.println(atmDept.getATM(0).getAtmname());
         System.out.println(atmDept.getATM(1).getAtmname());
+        System.out.println(atmDept.getATM(2).getAtmname());
         System.out.println("-------------");
         atmDept.printAllATMsMoney();
     }
