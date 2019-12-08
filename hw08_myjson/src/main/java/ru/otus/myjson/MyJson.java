6c3cd5b1449a474229ea5a1da9b82d96619e716b
VladimirLind <lindermon@gmail.com>
2019-12-08 17:31:58 +0300

    new changes

--- hw08_myjson/src/main/java/ru/otus/myjson/MyJson.java
+++ hw08_myjson/src/main/java/ru/otus/myjson/MyJson.java
@@ -1,53 +1,77 @@
 package ru.otus.myjson;
 
 import javax.json.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.*;
 
-public class MyJson {
+class MyJson {
 
-    JsonObjectBuilder mainbuilder = Json.createObjectBuilder();
-    JsonArrayBuilder arraybuilder = Json.createArrayBuilder();
-    String key = null;
+    String toJson(Object src) throws IllegalAccessException {
+        if (src == null) {
+            return JsonValue.NULL.toString();
+        }
+        return myJsonValue(src).toString();
+    }
 
-    public JsonObjectBuilder myJsonBuilder(Object src) {
-
-        if (src instanceof HashMap) {
-            HashMap<Object, Object> hmap = (HashMap<Object, Object>) src;
-            for (Map.Entry<Object, Object> entry : hmap.entrySet()) {
-                this.key = entry.getKey().toString();
-                myJsonBuilder(entry.getValue());
-            }
-        } else if (src.getClass().isArray()) {
-            Object[] arr = (Object[]) src;
-            for (int i = 0; i < arr.length; i++) {
-                myJsonBuilder(arr[i]);
-            }
-            nullKeyChecker(this.key);
-        } else if (src instanceof ArrayList) {
-            ArrayList<Object> arrlist = (ArrayList<Object>) src;
-            for (int i = 0; i < arrlist.size(); i++) {
-                myJsonBuilder(arrlist.get(i));
-            }
-            nullKeyChecker(this.key);
+    private JsonValue myJsonValue(Object src) throws IllegalAccessException {
+        Class clazz = src.getClass();
+        if (isWrapperType(clazz)) {
+             return primitiveChecker(src);
         } else if (src instanceof String) {
-            this.arraybuilder.add((String) src);
-        } else if (src instanceof Integer) {
-            this.arraybuilder.add((Integer) src);
+            return Json.createValue(src.toString());
+        } else if (clazz.isArray()) {
+            JsonArrayBuilder arraybuilder = Json.createArrayBuilder();
+            for (int i = 0; i < Array.getLength(src); i++) {
+                arraybuilder.add(myJsonValue(Array.get(src, i)));
+            }
+            return arraybuilder.build();
+        } else if (src instanceof Collection<?>) {
+            JsonArrayBuilder arraybuilder = Json.createArrayBuilder();
+            for (int i = 0; i < Array.getLength(((Collection) src).toArray()); i++) {
+                arraybuilder.add(myJsonValue(Array.get(((Collection) src).toArray(), i)));
+            }
+            return arraybuilder.build();
         } else {
-            System.out.println("no match");
+            JsonObjectBuilder objectbuilder = Json.createObjectBuilder();
+            for (Field field : src.getClass().getDeclaredFields()) {
+                field.setAccessible(true);
+                if (field.get(src) != null) {
+                    objectbuilder.add(field.getName(), myJsonValue(field.get(src)));
+                }
+            }
+            return objectbuilder.build();
         }
-        return this.mainbuilder;
     }
-    public String toMyJson(JsonObjectBuilder objectbuilder) {
-        return objectbuilder.build().toString();
-    }
-    private void nullKeyChecker(String key) {
-        if (this.key!=null) {
-            this.mainbuilder.add(this.key, this.arraybuilder);
-        } else {
-            this.mainbuilder.add("Null", this.arraybuilder);
+
+    private JsonValue primitiveChecker(Object src) {
+        if (src instanceof Integer) {
+            return Json.createValue((Integer) src);
+        } else if (src instanceof Double) {
+            return Json.createValue((Double) src);
+        } else if (src instanceof Float) {
+            return Json.createValue((Float) src);
+        } else if (src instanceof Long) {
+            return Json.createValue((Long) src);
+        } else if (src instanceof Character) {
+            return Json.createValue((Character) src);
+        } else if (src instanceof Byte) {
+            return Json.createValue((Byte) src);
+        } else if (src instanceof Short) {
+            return Json.createValue((Short) src);
+        } else if (src instanceof Boolean) {
+            return src.equals(true) ? JsonValue.TRUE : JsonValue.FALSE;
         }
+        return JsonValue.NULL;
+    }
+    private static boolean isWrapperType(Class<?> clazz) {
+        return clazz.equals(Boolean.class) ||
+                clazz.equals(Integer.class) ||
+                clazz.equals(Character.class) ||
+                clazz.equals(Byte.class) ||
+                clazz.equals(Short.class) ||
+                clazz.equals(Double.class) ||
+                clazz.equals(Long.class) ||
+                clazz.equals(Float.class);
     }
 }
--- hw08_myjson/src/main/java/ru/otus/myjson/TestMyJson.java
+++ hw08_myjson/src/main/java/ru/otus/myjson/TestMyJson.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class TestMyJson {
-    public static void main(String[] args) {
-        HashMap<String, ArrayList> hmap = new HashMap<>();
+    public static void main(String[] args) throws IllegalAccessException {
+//        HashMap<String, ArrayList> hmap = new HashMap<>();
         ArrayList<String> arlist = new ArrayList<>();
         arlist.add("1");
         arlist.add("2");
@@ -16,21 +16,16 @@ public class TestMyJson {
         arlist.add("4");
         arlist.add("55");
         arlist.add("66");
-        hmap.put("name", arlist);
-        hmap.put("name2", arlist);
-        hmap.put("name3", arlist);
-        hmap.put("name4", arlist);
-        hmap.put("name5", arlist);
-        System.out.println(hmap);
+        BagOfPrimitives obj1 = new BagOfPrimitives(22, "test", 10);
+        BagOfPrimitives obj2 = new BagOfPrimitives(23, "test", 11);
+        Boolean[] arr = {true, false, true};
+        ArrayList<BagOfPrimitives> arlist2 = new ArrayList<>();
+        arlist2.add(obj1);
+        arlist2.add(obj2);
         MyJson myjson = new MyJson();
-        JsonObjectBuilder ob2 = myjson.myJsonBuilder(hmap);
-        String json = myjson.toMyJson(ob2);
-        System.out.println(json);
-
-        Gson gson = new Gson();
-        HashMap obj2 = gson.fromJson(json, HashMap.class);
-        System.out.println(obj2);
-        System.out.println(obj2.equals(hmap));
-
+        System.out.println(myjson.toJson(arlist2));
+        System.out.println(myjson.toJson(arlist));
+        System.out.println(myjson.toJson(arr));
+        System.out.println(myjson.toJson(null));
     }
 }
