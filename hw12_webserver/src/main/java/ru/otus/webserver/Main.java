0f2fee309d0374d5981ed66a792dc9fa68f386fe
VladimirLind <lindermon@gmail.com>
2020-02-05 08:25:50 +0300

    DB initiation put into a class

--- hw12_webserver/src/main/java/ru/otus/webserver/Main.java
+++ hw12_webserver/src/main/java/ru/otus/webserver/Main.java
@@ -5,9 +5,8 @@ import ru.otus.webserver.api.dao.UserDao;
 import ru.otus.webserver.api.model.AddressDataSet;
 import ru.otus.webserver.api.model.PhoneDataSet;
 import ru.otus.webserver.api.model.User;
-import ru.otus.webserver.api.service.DBServiceUser;
-import ru.otus.webserver.api.service.DbServiceUserImpl;
 import ru.otus.webserver.hibernate.HibernateUtils;
+import ru.otus.webserver.hibernate.InitiateDB;
 import ru.otus.webserver.hibernate.dao.UserDaoHibernate;
 import ru.otus.webserver.hibernate.sessionmanager.SessionManagerHibernate;
 import ru.otus.webserver.server.UsersWebServer;
@@ -16,9 +15,6 @@ import ru.otus.webserver.services.TemplateProcessor;
 import ru.otus.webserver.services.TemplateProcessorImpl;
 import ru.otus.webserver.services.UserAuthService;
 import ru.otus.webserver.services.UserAuthServiceImpl;
-
-import java.util.HashSet;
-import java.util.Set;
 
 
 public class Main {
@@ -32,43 +28,8 @@ public class Main {
 
         SessionManagerHibernate sessionManager = new SessionManagerHibernate(sessionFactory);
         UserDao userDao = new UserDaoHibernate(sessionManager);
-        DBServiceUser dbServiceUser = new DbServiceUserImpl(userDao);
 
-        User user1 = new User(0, "Вася");
-        User user2 = new User(0, "Петя");
-        User user3 = new User(0, "Коля");
-
-        AddressDataSet address1 = new AddressDataSet("улица Мира, дом 1");
-
-        AddressDataSet address2 = new AddressDataSet("улица Мира, дом 2");
-
-        AddressDataSet address3 = new AddressDataSet("улица Мира, дом 3");
-
-        Set<PhoneDataSet> phoneDataSets1 = new HashSet<>();
-        phoneDataSets1.add(new PhoneDataSet("1111"));
-        phoneDataSets1.add(new PhoneDataSet("2222"));
-
-        Set<PhoneDataSet> phoneDataSets2 = new HashSet<>();
-        phoneDataSets2.add(new PhoneDataSet("3333"));
-        phoneDataSets2.add(new PhoneDataSet("4444"));
-
-        Set<PhoneDataSet> phoneDataSets3 = new HashSet<>();
-        phoneDataSets3.add(new PhoneDataSet("5955"));
-        phoneDataSets3.add(new PhoneDataSet("6666"));
-        phoneDataSets3.add(new PhoneDataSet("7777"));
-
-        user1.setAddressDataSet(address1);
-        user1.setPhoneDataSet(phoneDataSets1);
-        user2.setAddressDataSet(address2);
-        user2.setPhoneDataSet(phoneDataSets2);
-        user3.setAddressDataSet(address3);
-        user3.setPhoneDataSet(phoneDataSets3);
-
-        user3.setPassword("11111");
-
-        long id1 = dbServiceUser.saveUser(user1);
-        long id2 = dbServiceUser.saveUser(user2);
-        long id3 = dbServiceUser.saveUser(user3);
+        InitiateDB initiateDB = new InitiateDB(userDao);
 
         UserAuthService userAuthServiceForFilterBasedSecurity = new UserAuthServiceImpl(userDao);
 
